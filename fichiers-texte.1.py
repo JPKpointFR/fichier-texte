4e8a3d67583e59e0d71e991617342b004843b015
Adama Cissé <adam.cis345@gmail.com>
2022-10-25 21:45:12 +0200

    fichier texte

---
+++ fichiers-texte.1.py
@@ -0,0 +1,12 @@
+# FICHIERS TEXTE
+#
+# ouvrir (open) <- nom du fichier, mode
+# écrire (write) / lire (read)
+# fermer (close)
+#
+
+f = open("mon_fichier.txt", "w")
+
+f.write("Salam")
+
+f.close()
