21618eeff0eebeb6cfff8c28b48bb05dfaadf0fa
Adama Cissé <adam.cis345@gmail.com>
2022-10-26 18:13:48 +0200

    ,,,

--- fichiers-texte.1.py
+++ fichiers-texte.1.py
@@ -6,7 +6,15 @@
 #
 
 f = open("mon_fichier.txt", "w")
+f2 = open("mon_fichier2.txt", "w")
 
-f.write("Salam")
+# f.write("Bonjour\n")
+# f.write("Bonjour")
+
+
+l = ["phrase 1", "phrase 2", "phraseut"]
+
+f.write("\n".join(l))
+f.write("\nFin")
 
 f.close()
